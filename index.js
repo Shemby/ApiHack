8b2c1f93eb1e1ade9b4131bb1e6d2c50995f1c1c
Samuel Hemby <hembysam4@gmail.com>
2019-05-29 21:17:17 -0500

    added daily verse feature

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const apiKey = 'e905c2f122905efe6cee397e81d3904a';
-const baseUrl = 'https://api.scripture.api.bible/v1/bibles/06125adad2d5898a-01/search?query='
+const baseUrl = 'https://api.scripture.api.bible/v1/bibles/9879dbb7cfe39e4d-04'
 const myHeaders = new Headers();
       myHeaders.append('api-key', 'e905c2f122905efe6cee397e81d3904a');
 
-function getResults(userInput){
-    const url = `${baseUrl}${userInput}`;
-    console.log(url);
-    
+function getSearch(userInput){
+    $('.daily').addClass('hidden');
+    const url = `${baseUrl}/search?query=${userInput}`;
+    $('#results').removeClass('hidden');
     fetch(url,{
         headers: myHeaders
     })
@@ -18,23 +17,55 @@ function getResults(userInput){
             }
         throw new Error(response.statusText);
         })
-    .then(display)
+    .then(displaySearch)
     .catch(err => {
         $('#error-message').text(`something went wrong: ${err.message}`);
     });
 }
-function display(responseJson){
+function getChapter(event){
+    event.preventDefault();
+    const chapterLink = $(event.target);
+    const chapterId = chapterLink.data('chapter');
+    const url =`${baseUrl}/chapters/${chapterId}`;
+    fetch(url,{
+        headers: myHeaders
+    })
+        .then(response =>{
+            if(response.ok){
+                return response.json();
+            }
+        throw new Error(response.statusText);
+        })
+    .then(displayChapter)
+    .catch(err => {
+        $('#error-message').text(`something went wrong: ${err.message}`);
+    });
+
+}
+function displayChapter(responseJson){
     console.log(responseJson);
-    $('#resultsList').empty();
+    $('#results').addClass('hidden');
+    $('#resultsList').html(`<li class='chapterResponse'><h1>${responseJson.data.reference}</h1>
+    <p>${responseJson.data.content}</p></li>`);
+}
+function displaySearch(responseJson){
+    console.log(responseJson);
+    let totalResults = responseJson.data.total;
+    if (totalResults>0){
     $('#resultsList').html(responseJson.data.verses.map(o => {
-        return `<h1>${o.reference}:${o.text}</h1>`;
+        return `<li><h3>${o.reference}</h3><p>"${o.text}"</p><a href=""
+        class='chapter-link' data-chapter='${o.chapterId}'>view entire chapter</a></li>`;
     }));
 }
+    else{
+        $('#resultsList').html(`<h3>Your search returned no results.</h3>`)
+}}
 function listen() {
     $('form').submit (event => {
         event.preventDefault();
         const userInput = $('#searchQuery').val();
-        getResults(userInput);
+        getSearch(userInput);
     })
+    $('#resultsList').on('click', '.chapter-link', getChapter);
 }
 $(listen);
---
+++ verse.js
@@ -0,0 +1,61 @@
+'use-strict';
+
+const VERSES = [
+    'ROM.8.28',
+    'PHP.3.8',
+    '2CO.4.17',
+    'EPH.2.10',
+    'ROM.3.23',
+    'PHP.1.21',
+    'PSA.46.1',
+    'LUK.12.25',
+    'JHN.14.27',
+    'JOS.1.9',
+    '2TI.1.7',
+    '2COR.12.9',
+    'HEB.11.1',
+    '1PE.1.13',
+    'ISA.41.10',
+    'JOL.3.16',
+    'REV.21.4',
+    '2CO.5.17',
+    'MAT.11.28',
+    'EXO.34.6',
+    'EPH.2.4',
+    'HEB.4.14',
+    'PHP.4.6',
+    'JHN.16.33',
+    'DEU.10.17',
+    '1SA.12.22',
+    'LAM.3.22',
+    '1PE.1.3',
+    'JAS.1.22',
+    'COL.3.17',
+    'PHP.2.3'
+];
+function getDaily(){
+    let today = new Date();
+    let dd = (today.getDate());
+    let todaysVerse = VERSES[dd-1];
+    console.log(todaysVerse);
+    let url = `${baseUrl}/verses/${todaysVerse}`;
+    fetch(url,{
+        headers: myHeaders
+    })
+        .then(response =>{
+            if(response.ok){
+                return response.json();
+            }
+        throw new Error(response.statusText);
+        })
+    .then(displayDaily)
+    .catch(err => {
+        $('#error-message').text(`something went wrong: ${err.message}`);
+    });
+}
+function displayDaily(responseJson){
+    console.log(responseJson);
+    $('#dailyVerse').html(`<li><h1>${responseJson.data.reference}</h1>
+    <p>${responseJson.data.content}</p></li>`);
+}
+$(getDaily);
